cc24e4fcbf01ba2a9f5f6278d3fc471103b3fd5a
DGladiator <robiulhasan699@gmail.com>
2021-08-09 14:38:13 +0600

    modal close btn color

--- resources/views/fn/index.blade.php
+++ resources/views/fn/index.blade.php
@@ -145,7 +145,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-common" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
--- resources/views/fn/service.blade.php
+++ resources/views/fn/service.blade.php
@@ -114,7 +114,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-common" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
